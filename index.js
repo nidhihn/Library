5a2468c1760314e864227cfd6f49d64f4f095631
Nidhi H N <nidhinshetty28@gmail.com>
2023-11-08 15:54:37 +0530

    Added queryselector for login form in js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ document.querySelector('#search-btn').onclick=() =>{
     searchForm.classList.toggle('active');
 }
 
+let loginForm=document.querySelector('login-form-container');
+document.querySelector('#login-btn').onclick=()=>{
+    loginForm.classList.toggle('active');
+}
 
 window.onscroll=() =>{
 
