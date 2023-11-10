92932ae7015ec704927845a953a07d34b887a1b1
Nidhi H N <nidhinshetty28@gmail.com>
2023-11-10 22:14:34 +0530

    Linked js to html

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ document.querySelector('#search-btn').onclick=() =>{
 }
 
 let loginForm=document.querySelector('login-form-container');
+
 document.querySelector('#login-btn').onclick=()=>{
+    loginForm.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick=()=>{
     loginForm.classList.toggle('active');
 }
 
