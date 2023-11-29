77212a28ac8dd19cd1053f3997abaa2a054ec500
Nidhi H N <nidhinshetty28@gmail.com>
2023-11-29 18:26:23 +0530

    Added sliding

--- index.js
+++ index.js
@@ -31,3 +31,22 @@ window.onload=() =>{
         document.querySelector('.header .header-2').classList.remove('active');       
     }
 }
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredslides:true,
+    autoplay: {
+      delay:9500,
+      disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,    
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
